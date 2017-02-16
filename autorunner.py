66f0d5d1c95f9ef602b43e7ef01b743cc100682c
Ruben Putter <putterruben@gmail.com>
2017-02-16 14:05:00 +0200

    added a python file to do all the experiments consecutively

---
+++ autorunner.py
@@ -0,0 +1,13 @@
+import os.path
+import os
+import time
+
+pid = input("Enter the process ID: ")
+
+procPath = "/proc/" + str(pid)
+
+while os.path.exists(procPath):
+	time.sleep(1) #checks every 5 minutes if the process is still running 
+
+print("The thing has finished")
+os.system("pwd")
